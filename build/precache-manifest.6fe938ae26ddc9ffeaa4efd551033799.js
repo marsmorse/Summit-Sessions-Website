81f844562dacf51e8b9e17556361cfef67ae0caa
marsmorse <marsmorse@gmail.com>
2021-03-05 19:40:05 -0800

    [ADD] build folder

---
+++ build/precache-manifest.6fe938ae26ddc9ffeaa4efd551033799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5a739241c0175aafa175371ebf712c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73c8fbb4264fc1c7b04",
+    "url": "/static/js/2.c4afb893.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c4afb893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bdefe31c56a85c6bf9",
+    "url": "/static/js/main.46ed8bee.chunk.js"
+  },
+  {
+    "revision": "f68cd75fff4fa4a03d89",
+    "url": "/static/js/runtime-main.3a0fd5de.js"
+  }
+]);
